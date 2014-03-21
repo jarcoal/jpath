e34d2f83585e75f3e978100c0e90e0b49c41db02
Jared <jared@lytics.io>
2014-03-21 11:13:02 -0700

    tests

--- jpath_test.go
+++ jpath_test.go
@@ -94,8 +94,9 @@ func TestIndexSelector(t *testing.T) {
 
 func TestUnmarshal(t *testing.T) {
 	st := new(struct {
-		Color  string    `jpath:"$.store.bicycle.color"`
-		Prices []float64 `jpath:"$..price"`
+		Color       string    `jpath:"$.store.bicycle.color"`
+		Prices      []float64 `jpath:"$..price"`
+		FirstAuthor string    `jpath:"$..author"`
 	})
 
 	if err := Unmarshal(documentBytes, st); err != nil {
@@ -109,11 +110,23 @@ func TestUnmarshal(t *testing.T) {
 	if len(st.Prices) != 5 {
 		t.Fatalf("expected Prices to have 5 entries, got %v", len(st.Prices))
 	}
+
+	if st.FirstAuthor != "Nigel Rees" {
+		t.Fatalf("expected FirstAuthor to be '%s', got '%s'", st.FirstAuthor)
+	}
 }
 
 func TestInvalidUnmarshal(t *testing.T) {
+	// needs to be a struct
 	{
-		// price isn't a string
+		m := make(map[string]interface{})
+		if err := Unmarshal(documentBytes, &m); err == nil {
+			t.Fatal("expected an error to be returned")
+		}
+	}
+
+	// price is a float64, not a string
+	{
 		st := new(struct {
 			Price string `jpath:"$..price"`
 		})
@@ -122,8 +135,9 @@ func TestInvalidUnmarshal(t *testing.T) {
 			t.Fatal("expected an error to be returned")
 		}
 	}
+
+	// authors are strings, not floats
 	{
-		// authors are strings, not floats
 		st := new(struct {
 			Authors []float64 `jpath:"$..author"`
 		})
